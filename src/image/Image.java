6bfe82817705cdf7771d6a02ae0d76343422a741
Zen <zenlovehaha@foxmail.com>
2020-03-11 09:05:26 +0800

    图片路径外存

--- src/image/Image.java
+++ src/image/Image.java
@@ -2,12 +2,13 @@ package image;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 
 public class Image {
     public static BufferedImage imageLoad(String path){
         try {
-            return ImageIO.read(Image.class.getResource(path));
+            return ImageIO.read(new File("."+path));
         } catch (IOException e) {
             e.printStackTrace();
             System.exit(1);
